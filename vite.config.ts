669a49fb665da3ad071fde71e0b28580c3df148e
iRiceCrispy <huangeric.j@gmail.com>
2026-03-24 16:30:11 -0700

    feat(dev): enable HTTPS

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    mkcert(),
     react(),
     tailwindcss(),
   ],
