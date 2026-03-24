f6fc0131991b88f9f69df8cd7f6c1fe0c3813028
iRiceCrispy <huangeric.j@gmail.com>
2026-03-24 16:00:44 -0700

    chore: add ESlint plugins and rules
    - eslint-plugin-import-x
    - @stylistic/eslint-plugin

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,10 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import { reactRefresh } from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import { configs as tsConfigs, parser as tsParser } from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
+import { importX } from 'eslint-plugin-import-x'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -11,13 +13,54 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tsConfigs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite(),
+      importX.flatConfigs.recommended,
+      importX.flatConfigs.typescript,
+      stylistic.configs.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tsParser,
+    },
+    rules: {
+      'eqeqeq': ['error', 'smart'],
+      'no-empty': ['error', { allowEmptyCatch: true }],
+      '@typescript-eslint/no-empty-object-type': ['error', { allowInterfaces: 'with-single-extends' }],
+      '@typescript-eslint/no-unused-vars': [
+        'error', {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      'import-x/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+      'import-x/exports-last': 'error',
+      'import-x/first': 'error',
+      'import-x/newline-after-import': 'error',
+      'import-x/order': [
+        'error', {
+          'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+          'distinctGroup': false,
+          'newlines-between': 'never',
+          'alphabetize': { order: 'asc', caseInsensitive: true },
+          'named': true,
+          'sortTypesGroup': true,
+        },
+      ],
+      '@stylistic/array-bracket-newline': 'error',
+      '@stylistic/array-element-newline': ['error', 'consistent'],
+      '@stylistic/curly-newline': ['error', { multiline: true, consistent: true }],
+      '@stylistic/function-call-argument-newline': ['error', 'consistent'],
+      '@stylistic/function-call-spacing': 'error',
+      '@stylistic/function-paren-newline': ['error', 'multiline-arguments'],
+      '@stylistic/jsx-self-closing-comp': 'error',
+      '@stylistic/object-curly-newline': ['error', { multiline: true, consistent: true }],
+      '@stylistic/one-var-declaration-per-line': 'error',
+      '@stylistic/switch-colon-spacing': 'error',
     },
   },
 ])
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { resolve } from 'path'
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
